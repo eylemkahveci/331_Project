0a3bf435033f461344901d9d6bd57acfe1a54dc9
EmreUgurA <90784006+EmreUgurA@users.noreply.github.com>
2024-12-22 14:12:24 +0300

    proje gelistirme

---
+++ ACE/ACE/ShoppingCartView.swift
@@ -0,0 +1,18 @@
+//
+//  ShoppingCartView.swift
+//  ACE
+//
+//  Created by Emre Uğur on 17.12.2024.
+//
+
+import SwiftUI
+
+struct ShoppingCartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShoppingCartView()
+}
